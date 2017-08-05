88441314adb096adebd541d5f26558d31adb427a
wajid003 <abdulwajid764@gmail.com>
2017-08-05 14:27:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/ui/index/robot.html',function(req, res){
-    res.sendFile(path.join(_dirname, 'ui','robot.html'));
+    res.sendFile(path.join(_dirname, 'index','robot.html'));
 });
 
 
