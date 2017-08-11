33ba395b7649cd8c57d61b293408d4a5d7236130
wajid003 <abdulwajid764@gmail.com>
2017-08-11 22:31:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,5 @@ button.onclick=function(){
     
     counter=counter + 1;
     var span=document.getElementById('count');
-    span.innerHTML= counter.toString();
+    span.innerHTML = counter.toString();
 };
