6fcaa698b21eba725236cdf2f364b83176381eac
wajid003 <abdulwajid764@gmail.com>
2017-08-11 22:19:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/counter',function(req,res){
+    counter = counter+1;
+    res.send(counter.toString());
+    
+});
 app.get('/ui/main.js', function (req,res){
     res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
