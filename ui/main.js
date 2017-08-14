38d71553232f85650a64d43ac97fe9f634921316
wajid003 <abdulwajid764@gmail.com>
2017-08-14 11:47:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,6 +14,24 @@ button.onclick = function(){
 request.open('GET','http://abdulwajid764.imad.hasura-app.io/counter',true);
 request.send(null);
 };
+ var text = document.getElementById("text");
+            var post = document.getElementById("post");
+            post.onclick = function(){
+                var request = new XMLHttpRequest();
+                request.onreadystatechange = function(){
+                  if(request.readyState == XMLHttpRequest.DONE){
+                    if(request.status ==200){
+                        var text2 = request.responseText;
+                        text2 = JSON.parse(text2);
+                        var print = document.getElementById("print");
+                        print.innerHTML = text2.value;
+            }
+                  }
+                };
+                 var text1 = text.value;
+                request.open('GET','http://abdulwajid764.imad.hasura-app.io/article.html' +text1,true);
+                 request.send(null);
+};
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
@@ -39,3 +57,4 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 request.open('GET', 'http://abdulwajid764.imad.hasura-app.io/submit-name?name?' +name , true);
 request.send(null);
+};
