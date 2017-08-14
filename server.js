9349f69c90b077379cc9ac4fca8b2d382f9cf8c2
wajid003 <abdulwajid764@gmail.com>
2017-08-14 12:23:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,9 +6,18 @@ var app = express();
 
 app.use(morgan('combined'));
 
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/comment.html',function (req,res){
+    res.send("comment page");
+});
+app.get('/post.html',function (req,res){
+    res.send("post page");
+});
+
 var counter = 0;
 app.get('/counter',function(req,res){
     counter = counter + 1;
