aafd724697144885533182df5f3e5f7a6b1d9b3a
wajid003 <abdulwajid764@gmail.com>
2017-08-26 11:24:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config = {
     database: 'abdulwajid764',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB.PASSWORD
+    password: process.env.DB_PASSWORD
 };
 app.use(morgan('combined'));
 
