6aea9ce2e205893da81c834cb6291cf4fa79df37
wajid003 <abdulwajid764@gmail.com>
2017-08-26 11:23:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool = require('pg').pool;
+var Pool = require('pg').Pool;
 var app = express();
 var pool = new Pool(config);
 var config = {
