d3fdfb041ba85be80708b4ea429aa18acd0ec83f
wajid003 <abdulwajid764@gmail.com>
2017-08-27 11:39:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,15 @@ var config = {
 app.use(morgan('combined'));
 
 
-
+function hash (input, salt) {
+    //how do we create hash
+    var hashed = crypto.pbkdf25ync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input', function(req, res){
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
